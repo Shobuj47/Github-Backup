93ba53392f124879ef227ed154065d976bcaaf2b
shobuj47 <shobuj.akash1@gmail.com>
2021-11-15 22:45:07 +0600

    Backup GitHub with Linux Shell Script
    Backup GitHub with Linux Shell Script

---
+++ git-backup.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+#--------------------------------------------------------------------------------
+#                _   _                       _____ _           _           _ 
+#     /\        | | | |                 _   / ____| |         | |         (_)
+#    /  \  _   _| |_| |__   ___  _ __  (_) | (___ | |__   ___ | |__  _   _ _ 
+#   / /\ \| | | | __| '_ \ / _ \| '__|      \___ \| '_ \ / _ \| '_ \| | | | |
+#  / ____ \ |_| | |_| | | | (_) | |     _   ____) | | | | (_) | |_) | |_| | |
+# /_/    \_\__,_|\__|_| |_|\___/|_|    (_) |_____/|_| |_|\___/|_.__/ \__,_| |
+#                                                                        _/ |
+#                                                                       |__/ 
+#               __       _______ 
+#|\     /|     /  \     (  __   )
+#| )   ( |     \/) )    | (  )  |
+#| |   | | _____ | |    | | /   |
+#( (   ) )(_____)| |    | (/ /) |
+# \ \_/ /        | |    |   / | |
+#  \   /       __) (_ _ |  (__) |
+#   \_/        \____/(_)(_______)
+#                                
+#-----------------------------------------------------------------------------------
+
+# User Variables:
+username="shobuj47"
+usertoken=""
+backupDir="/root/git_backup"
+
+
+
+
+mkdir -p $backupDir
+if [ -d $backupDir ] ; then
+        echo "Backup directory exists"
+else
+        echo "Backup directory does not exists"
+	exit 0
+fi
+
+if [[ -z ${usertoken} ]]; then
+repoUrl="https://api.github.com/users/$username/repos"
+else
+repoUrl="https://api.github.com/users/$username/repos?access_token=$usertoken"
+fi
+
+echo $repoUrl
+repoList=$(curl $repoUrl | jq -r ".[].clone_url" )
+echo "Repolist"
+echo "========================================================================"
+while IFS= read -r repoUrl; do
+	repoDir=$(echo $repoUrl | awk -F '/' '{ print $NF }' )
+	cloneDir="${backupDir}/${repoDir}"
+	echo "Repository Url: $repoUrl"
+	echo "Clone Dir: $cloneDir"
+	git clone $repoUrl $cloneDir
+	echo "Pulling All Branches"
+	cd $cloneDir/ && git pull --all
+done <<< "$repoList"
+
